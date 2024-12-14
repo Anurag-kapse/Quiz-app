6fbfae02c105c5cde03be10455fdc97344eb1635
Anurag Kapse <anuragkapse009@gmail.com>
2024-12-14 12:31:11 +0530

    second commit

---
+++ src/assets/data/data.jsx
@@ -0,0 +1,89 @@
+export const data = [
+  {
+    Question:
+      " Which of the following command is used to create react-js-app ?",
+    Option1: "npx create-react-app appname",
+    Option2: "npm install create-react-app",
+    Option3: "npx install create-react-app -g",
+    Option4: "install - l create-react-app",
+
+    ans: 1,
+  },
+
+  {
+    Question:
+      "In React.js which one of the following is used to create a class for Inheritance ?",
+    Option1: "Create",
+    Option2: "Extends",
+    Option3: "Inherits",
+    Option4: "Delete",
+
+    ans: 2,
+  },
+
+  {
+    Question: "What is the default port number in which the application run ?",
+    Option1: "3000",
+    Option2: "8080",
+    Option3: "5000",
+    Option4: "3030",
+
+    ans: 1,
+  },
+
+  {
+    Question: "Which of the following valid component return type of React ? ",
+    Option1: "2",
+    Option2: "3",
+    Option3: "1",
+    Option4: "5",
+
+    ans: 3,
+  },
+  {
+    Question: "Which of the following is a way to handle data in React.js ?",
+    Option1: "State & Props",
+    Option2: "Services & Components",
+    Option3: "State & Services",
+    Option4: "State & Component",
+
+    ans: 1,
+  },
+  {
+    Question: "Which of the following is must for the API in React.js ?",
+    Option1: "SetinitialComponent",
+    Option2: "renderComponent",
+    Option3: "render",
+    Option4: "All of the above",
+
+    ans: 2,
+  },
+  {
+    Question: "Which of the following is true regarding Babel ?",
+    Option1: "Compiler",
+    Option2: "Transpilar",
+    Option3: "Both of the above",
+    Option4: "None of the above",
+
+    ans: 3,
+  },
+  {
+    Question: "Which of the following is the correct name of React.js?",
+    Option1: "React",
+    Option2: "React.js",
+    Option3: "ReactJS",
+    Option4: "All of the above",
+
+    ans: 4,
+  },
+  {
+    Question:
+      " What of the following is used in React.js to increase performance?",
+    Option1: "Original DOM",
+    Option2: "Virtual DOM",
+    Option3: "Both A and B.",
+    Option4: "None of the above.",
+
+    ans: 2,
+  },
+];
